5f95e4ad0533d0a0ccc2a120d308ab5e5b5ddb59
Guillermo Cubells <guillermocubells@gmail.com>
2022-08-27 02:04:16 +0200

    feature:Styling form

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,10 +2,6 @@ const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  // let isLoggedIn = false;
-  // if (req.session.user) {
-  //   isLoggedIn = true;
-  // }
   res.render("homepage");
 });
 
